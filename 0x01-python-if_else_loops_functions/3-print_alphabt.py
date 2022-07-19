0793c8b1ec25542c7019ccadbe2f6752a71385db
Brucethegreat <sitibruce@gmail.com>
2022-07-19 21:13:13 +0000

    python class

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,5 @@ for b in range(97, 123, 1):
     if b == 113 or b == 101:
         pass
     else:
-        print("{}".format(chr(b)), end="")
+        print("{:c}".format(chr(b)), end="")
+abcdfghijklmnoprstuvwxyz>>>
